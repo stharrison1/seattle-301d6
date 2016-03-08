ab52af38cca041f75f428aa57c7cd0d5a7026739
Susan Harrison <susan77harrison@gmail.com>
2016-03-08 12:43:11 -0800

    Made changes per class lecture.

--- class-01-jquery-and-the-dom/susan-Jeffrey/scripts/article.js
+++ class-01-jquery-and-the-dom/susan-Jeffrey/scripts/article.js
@@ -12,24 +12,20 @@ function Article (opts) {
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
-
-  $newArticle.attr('data-category', this.category);
-
-  $newArticle.find('h1').html(this.title);
-  $newArticle.find('address a').attr('href', this.authorUrl);
-  $newArticle.find('address a').html(this.author);
-  $newArticle.find('.article-body').html(this.body);
-
-  // Include the publication date as a 'title' attribute to show on hover:
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
-
-  // Display the date as a relative number of "days ago":
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
-
-  $newArticle.append('<hr>');
-
-  // TODO: This cloned article is no longer a template, so we should remove that class...
   $newArticle.removeClass('template');
+  if (!this.publishedOn) { // if not this.publishedOn addClass draft
+    $newArticle.addClass('draft');
+  }
+  $newArticle.data('category', this.category);//setter assigning category with this. category
+  // $newArticle.attr('data-category', this.category);
+  $newArticle.find('h1:first').text(this.title);
+  $newArticle.find('address a').html(this.author);
+  $newArticle.find('address a').attr('href', this.authorUrl);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn);
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
+  $newArticle.append('<hr>');
   return $newArticle;
 }
 
